c4dcb1f1e099736d6fb0eb7c0e96f7f2d32d4800
adrianopatrick@gmail.com <adrianopatrick@gmail.com@b2f47c30-29ef-f710-9018-d384b9bebada>
2012-11-01 00:20:58 +0000

    patrick cunha - layout padrão

--- CaixeiroViajanteGuloso/src/br/uece/caixeiroviajanteguloso/heuristica/Main.java
+++ CaixeiroViajanteGuloso/src/br/uece/caixeiroviajanteguloso/heuristica/Main.java
@@ -1,10 +1,6 @@
 package br.uece.caixeiroviajanteguloso.heuristica;
 
-import java.io.File;
-import java.net.URL;
-import java.util.List;
-
-import br.uece.caixeiroviajanteguloso.utils.ArquivoUtils;
+import br.uece.caixeiroviajanteguloso.ui.frmPrincipal;
 
 
 /**
@@ -12,47 +8,47 @@ import br.uece.caixeiroviajanteguloso.utils.ArquivoUtils;
  * */
 public class Main {
 	
-	/*public static void main(String[] args) {
+	public static void main(String[] args) {
 		frmPrincipal form = new frmPrincipal();
 		form.setVisible(true);
-	}*/
+	}
 
 // Main para calculo do tempo de execução
 //
-	public static void main(String[] args) {
-		
-		long inicio = System.nanoTime();
-		
-		File file = new File(new Main().getRelativePath().getFile()+"PCV26.txt");
-		Celula[][] matriz = new ArquivoUtils().lerArquivo(file);
-		ThreadGroup threadGroup = new ThreadGroup("caixeiro");
-		Fluxo[] fluxos = new Fluxo[matriz.length];
-		
-		for (int i = 0; i < fluxos.length; i++) {
-			fluxos[i] = new Fluxo(threadGroup, "Thr"+i, i, file);
-			fluxos[i].start();
-			
-			System.gc();
-		}
-		
-		while(threadGroup.activeCount() != 0);
-		
-		List<Caminho> caminhos = Fluxo.getCaminhos();
-		for (Caminho caminho : caminhos) {
-			Ponto[] pontos = caminho.getCaminho();
-			for (Ponto ponto : pontos) {
-				System.out.print(ponto.getId()+" --> ");
-			}
-			
-			System.out.println("Distancia: " + caminho.getDistancia());
-		}
-		
-		long total = System.nanoTime() - inicio;
-		System.out.println("tempo total: "+total/1000000000);
-		
-	}
-	
-	public URL getRelativePath(){
-		return getClass().getResource("/");
-	}
+//	public static void main(String[] args) {
+//		
+//		long inicio = System.nanoTime();
+//		
+//		File file = new File(new Main().getRelativePath().getFile()+"PCV26.txt");
+//		Celula[][] matriz = new ArquivoUtils().lerArquivo(file);
+//		ThreadGroup threadGroup = new ThreadGroup("caixeiro");
+//		Fluxo[] fluxos = new Fluxo[matriz.length];
+//		
+//		for (int i = 0; i < fluxos.length; i++) {
+//			fluxos[i] = new Fluxo(threadGroup, "Thr"+i, i, file);
+//			fluxos[i].start();
+//			
+//			System.gc();
+//		}
+//		
+//		while(threadGroup.activeCount() != 0);
+//		
+//		List<Caminho> caminhos = Fluxo.getCaminhos();
+//		for (Caminho caminho : caminhos) {
+//			Ponto[] pontos = caminho.getCaminho();
+//			for (Ponto ponto : pontos) {
+//				System.out.print(ponto.getId()+" --> ");
+//			}
+//			
+//			System.out.println("Distancia: " + caminho.getDistancia());
+//		}
+//		
+//		long total = System.nanoTime() - inicio;
+//		System.out.println("tempo total: "+total/1000000000);
+//		
+//	}
+//	
+//	public URL getRelativePath(){
+//		return getClass().getResource("/");
+//	}
 }
